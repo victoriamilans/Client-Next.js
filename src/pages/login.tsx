f44d01f450dd7d5049afba93708df09737d6d602
Victoria <victoria-milan@outlook.com>
2023-03-24 20:59:54 -0300

    feat/ add inputs

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,32 @@
-const LoginPage = () => {
-  return <h1>oi</h1>;
+import api from "@/services/api";
+import { IClientLogin, IProps } from "@/types";
+import { GetServerSideProps, NextPage } from "next";
+
+const LoginPage: NextPage<IProps> = () => {
+  return (
+    <>
+      <div>
+        <h1>Weslcome back</h1>
+        <span>Welcome back! Please enter your details</span>
+        <form>
+          <input type={"email"} id={"email"} />
+          <input type={"password"} id={"password"} />
+          <button>Login</button>
+          <button>Login</button>
+        </form>
+      </div>
+      <div></div>
+    </>
+  );
 };
 
 export default LoginPage;
+
+export const getServiceSideProps: GetServerSideProps<
+  IClientLogin
+> = async () => {
+  const response = await api.post("/login");
+  const client = response.data;
+
+  return { props: client };
+};
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface IClientLogin {
+  email: string;
+  password: string;
+}
+
+export interface IProps {
+  token: string;
+}
